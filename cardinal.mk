134a69e49bd37fd58593a12195e98b5b0d80f2bf
althafvly <althafvly@gmail.com>
2017-12-26 13:35:25 +0000

    Cardinal aosp bringup

--- cardinal.mk
+++ cardinal.mk
@@ -25,7 +25,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := osprey
-PRODUCT_NAME := lineage_osprey
+PRODUCT_NAME := cardinal_osprey
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := osprey
